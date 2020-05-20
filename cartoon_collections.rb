14decac6a4339b3a1f71e4838872a2d4ac987358
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-20 04:39:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   index + 1 
   array.each_with_index do |element, index|
-    puts "#{index} #{element}"
+    puts "#{index + 1 } #{element}"
     end
     list_dwarves
 end
